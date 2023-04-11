038949f50ca0ed4ea7af584726eecd2e6a9289d0
MarecoAJ <alejo_mareco@hotmail.com>
2023-04-11 10:35:01 -0300

    id para manipulacion de cabecero en html. arreglos y func para refrescar info

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const ingresos =[
+    new Ingreso("Salario",2100.00),
+    new Ingreso("venta coche", 1500)
+];
+
+const egresos = [
+    new Egreso("renta", 900),
+    new Egreso("ropa", 400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let cargarCabecero = ()=>{
+
+}
