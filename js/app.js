5809858f2b49ec305af9bde4da5d7b9a52c7ece6
MarecoAJ <alejo_mareco@hotmail.com>
2023-04-11 11:12:07 -0300

    carga de presupuesto, egreso e ingreso en html dinamico

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-const ingresos =[
-    new Ingreso("Salario",2100.00),
+const divPresupuesto = document.getElementById("presupuesto");
+const divPorcentaje = document.getElementById("porcentaje");
+const divIngresos = document.getElementById("ingresos");
+const divEgresos = document.getElementById("egresos");
+
+const ingresos = [
+    new Ingreso("Salario", 2100.00),
     new Ingreso("venta coche", 1500)
 ];
 
@@ -8,10 +13,36 @@ const egresos = [
     new Egreso("ropa", 400)
 ];
 
-let cargarApp = ()=>{
+let cargarApp = () => {
     cargarCabecero();
 }
 
-let cargarCabecero = ()=>{
+let totalIngresos = () => {
+    let totalIngreso = 0;
 
+    for (let ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
+    }
+
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+
+    for (let egreso of egresos) {
+        totalEgreso += egreso.valor;
+    }
+
+    return totalEgreso;
+}
+
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
+    divPresupuesto.innerHTML = presupuesto; 
+    divPorcentaje.innerHTML = porcentajeEgreso;  
+    divIngresos.innerHTML = totalIngresos();
+    divEgresos.innerHTML = totalEgresos();
 }
